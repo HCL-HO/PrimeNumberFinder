62487f26e1ff19debda9bc5729c275bfbb093157
Eric CL HO <clhoac@gamil.com>
2017-06-16 16:02:28 +0800

    Final

--- src/Calculation/Finder.java
+++ src/Calculation/Finder.java
@@ -13,6 +13,14 @@ import java.util.List;
 import Util.WriteFile;
 
 public class Finder {
+	/*
+	 * 1) INIT- Load each line from record to initialize
+	 * 2) BUSSINESS- getNumbers to perform business algorithm
+	 * 3) READEXISTINGDATA- loadListByBatch to read existing
+	 * 4) WRITENEWDATA- batchWriteLog in the BUSSINESS to record the new data
+	 * 
+	 *  
+	 */
 	List<Integer> numList = new ArrayList<Integer>();
 	int newNum;
 	WriteFile writer = new WriteFile();
@@ -26,15 +34,15 @@ public class Finder {
 		String fullMes = "";
 		String[] nums;
 		try {
-			BufferedReader BR =  new BufferedReader(new FileReader(new File("PrimeNumber")));
-			line = BR.readLine();
-			while(line != null){
-				fullMes += line;
-				line = BR.readLine();
-			}
-			BR.close();	
+//			BufferedReader BR =  new BufferedReader(new FileReader(new File("PrimeNumber")));
+//			line = BR.readLine();
+//			while(line != null){
+//				fullMes += line;
+//				line = BR.readLine();
+//			}
+//			BR.close();	
 			
-			BR =  new BufferedReader(new FileReader(new File("TheLastNum")));
+			BufferedReader BR =   new BufferedReader(new FileReader(new File("TheLastNum")));
 			line = BR.readLine();
 			if(line != null){
 				theLastNum = Integer.parseInt(line.split(",")[0]);
@@ -44,14 +52,13 @@ public class Finder {
 			nums = fullMes.split(",");
 			System.out.println("Numbers Stored: "+ nums.length);
 			
-			for(String s : nums){
-				int num = (s.isEmpty())? 2 : Integer.parseInt(s);
-				numList.add(num);
-				if(num > theLastNum){
-					break;
-				}
-			}
-
+//			for(String s : nums){
+//				int num = (s.isEmpty())? 2 : Integer.parseInt(s);
+//				numList.add(num);
+//				if(num > theLastNum){
+//					break;
+//				}
+//			}
 			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -62,16 +69,16 @@ public class Finder {
 		
 	}
 	
-	public void getNumbers(List<Integer> list, int batch){
-		numList = list;
+	public void getNumbers(int batch){
+//		numList = list;
 		System.out.println("--------------getNumbers---------------------");
 		Collections.sort(numList);
 		newNum = theLastNum;
 		int found = 0;
 		int size = batch * batchSize;
 		while(found < size){
-			if(proccessor(list, newNum)){
-				list.add(newNum);
+			if(proccessor(newNum)){
+				numList.add(newNum);
 				System.out.println("Number found:" + newNum);
 				newNum++;
 				found++;
@@ -87,28 +94,77 @@ public class Finder {
 		if(found % batchSize == 0){
 			// make String
 			String results ="";
-			int startNum = (batchNum*batchSize == 0)? 0: batchNum*batchSize +1;
-			for(int i = startNum ;  i<= (batchNum+1)*batchSize; i++){
+//			int startNum = (batchNum*batchSize == 0)? 0: (batchNum*batchSize) +1;
+			for(int i = 0 ; i< numList.size(); i++){
 				results += String.valueOf(numList.get(i)) + ",";
 			}
-			System.out.println("--------------Batch: "+ batchNum);
+			System.out.println("--------------Finish Batch: "+ batchNum+ "---------------------------");
 			batchNum++;
 			writer.writeLastNum(String.valueOf(numList.get(numList.size()-1))+","+String.valueOf(batchNum));
 			writer.write(results);
+			numList.clear();
 		}
 	}
 
-	private boolean proccessor(List<Integer> list, int newNum) {
+	private boolean proccessor(int newNum) {
 //		System.out.println("--------------proccessor---------------------");
 //		System.out.println("newNumb: " + newNum);
+		BufferedReader BR;
+		try {
+		BR = new BufferedReader(new FileReader(new File("PrimeNumber")));
+		List<Integer> list = loadListByBatch(BR, 2);
 		int i = 0;
+		
 		while(newNum % list.get(i) != 0){
-			if(list.size()-1 == i){
-				return true;
-			} else {
-				i++;
+				if(list.size()-1 == i){
+					list = loadListByBatch(BR, 2);
+					i = 0;
+					if(list.size()==0){
+						return true;
+					}
+				} else {
+					i++;
+				}
 			}
+		
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 		return false;
+	}
+	
+	public List<Integer> loadListByBatch(BufferedReader BR, int batchSize){
+		String line;
+		int lineNum = 1;
+		String mes = "";
+		String[] nums = new String[]{};
+		List<Integer> numsList = new ArrayList<Integer>();
+		try {
+			while((line=BR.readLine())!=null){
+				System.out.println("loadListByBatch: StartLoop");
+					if(line.isEmpty()){
+						System.out.println("loadListByBatch: Empty line");
+					} else {
+						System.out.println("loadListByBatch: Valid line");
+						mes += line;
+						lineNum++;
+						if(lineNum>batchSize){
+							break;
+						}
+					}
+			}
+			if(mes!=null){
+				System.out.println("loadListByBatch: Finish");
+				nums = mes.split(",");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		for(String s: nums){
+			if(!s.isEmpty()){
+				numsList.add(Integer.parseInt(s));	
+			}
+		}
+		return numsList;
 	}
 }
--- src/Main/Starter.java
+++ src/Main/Starter.java
@@ -1,5 +1,14 @@
 package Main;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.swing.text.DateFormatter;
+
 import Calculation.Finder;
 
 public class Starter {
@@ -7,9 +16,31 @@ public class Starter {
 	public static void main(String[] args) {
 		
 //		new Finder().loadNumbers();
+		SimpleDateFormat DF = new SimpleDateFormat("hh:mm:ss");
+		long start = System.currentTimeMillis();
 		
 		Finder find = new Finder();
-		find.getNumbers(find.loadNumbers(), 100);
+
+		find.loadNumbers();
+		find.getNumbers(1);
+		
+		System.out.println("Time taken: "+ (System.currentTimeMillis()- start)/1000 + "/sec");
+			
+//			System.out.println("--------------------------------------------");
+//			List<Integer> list1 = find.loadListByBatch(BR, 2);
+//			for(int i : list1){
+//				System.out.println(i);
+//
+//			}
+//			
+//			System.out.println("--------------------------------------------");
+//			List<Integer> list2 = find.loadListByBatch(BR, 2);
+//			for(int i : list2){
+//				System.out.println(i);
+//
+//			}
+
+
 	}
 
 }
